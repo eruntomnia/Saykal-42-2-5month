ddb21a87e8a19d6c2763516233cf20084bbeb9b0
eruntomnia <ryskulovasaykal10@gmail.com>
2024-12-02 02:23:11 +0600

    name

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
-// import {createStore} from "redux";
-// import reducer from "./redux/reducer";
-//
-// const store = createStore(reducer)
 
 ReactDOM.render(
+    <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
